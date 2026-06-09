77cea06c33cd89c757247a5c6479777205df2437
unknown <dev.rafaelot@gmail.com>
2026-06-09 09:28:37 -0300

    fix prisma client

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,17 +1,9 @@
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-// Evita recriar conexões durante o desenvolvimento (Hot Reload)
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-// Configuração do Adapter (igual ao seu seed.ts)
-const connectionString = process.env.DATABASE_URL
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
